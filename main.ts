63704e2930390b6f7f8ab0d1c1b4ad0d200e4c83
Joemcflo13 <65471652+Joemcflo13@users.noreply.github.com>
2020-05-16 20:48:16 -0600

    Update pxt.json, main.ts

--- main.ts
+++ main.ts
@@ -14,5 +14,14 @@ namespace starterkit {
     export function buttonsetup(): void {
         pins.P1; PinPullMode.PullUp
     }
-
+    /**
+     * uses the trimpot to plot the screen
+     */
+    //% buttonId=starterkittrimpot button="trimpot P0"
+    export function trimpot(): void {
+        led.plotBarGraph(
+        pins.analogReadPin(AnalogPin.P0),
+        1023
+        )
+    }
 }
