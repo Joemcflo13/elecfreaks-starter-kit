8ffc0374e213a9cbcc2d94be437e1c3b14a7231c
Joemcflo13 <65471652+Joemcflo13@users.noreply.github.com>
2020-05-17 13:47:14 -0600

    Update main.ts

--- main.ts
+++ main.ts
@@ -19,17 +19,6 @@ namespace starterkit {
         pins.P1; PinPullMode.PullUp
     }
     /**
-     * uses the trimpot to plot the screen
-     */
-    //% weight=57
-    //% buttonId=starterkittrimpot button="trimpot P0"
-    export function trimpot(): void {
-        led.plotBarGraph(
-            pins.analogReadPin(AnalogPin.P0),
-            1023
-        )
-    }
-    /**
      * sets the rgb lights to any color
      */
     //% weight=34
