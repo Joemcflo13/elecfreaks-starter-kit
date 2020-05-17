87049b806e4007f0b7b5812b558c80d48e0ce8a9
Joemcflo13 <65471652+Joemcflo13@users.noreply.github.com>
2020-05-16 20:00:41 -0600

    Update pxt.json, main.ts

--- main.ts
+++ main.ts
@@ -1,11 +1,15 @@
 namespace starterkit {
     /**
-     * some of the esentials for the elecfreaks starter kit
+     * reacts with P2
      */
     //% blockId=starterkitbutton block="Button"
     export function button(): void {
-        
+        if (pins.P2 = 0)
+            
     }
+    /**
+     * the setup for a button
+     */
     //% blockId=starterkitbuttonsetup block="Button setup P2"
     export function buttonsetup(): void {
         pins.P2; PinPullMode.PullUp
