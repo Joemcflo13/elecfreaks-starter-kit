28662b2ad3b44f307379dae810768e323d21516b
Joemcflo13 <65471652+Joemcflo13@users.noreply.github.com>
2020-05-16 19:45:34 -0600

    Update main.ts

--- main.ts
+++ main.ts
@@ -1,1 +1,14 @@
+namespace starterkit {
+    /**
+     * some of the esentials for the elecfreaks starter kit
+     */
+    //% blockId=starterkitbutton block="Button"
+    export function button(): void {
+        
+    }
+    //% blockId=starterkitbuttonsetup block="Button setup P2"
+    export function buttonsetup(): void {
+        pins.P2; PinPullMode.PullUp
+    }
 
+}
