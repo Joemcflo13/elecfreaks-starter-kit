f7693801a88189ab656beb4bd538e7bc4d4700f2
Joemcflo13 <65471652+Joemcflo13@users.noreply.github.com>
2020-05-17 13:32:11 -0600

    new block

--- main.ts
+++ main.ts
@@ -4,8 +4,8 @@ namespace starterkit {
      */
     //% blockId=starterkitbutton block="Button P1"
     export function button(P1 = 1): Boolean {
-            true
-            return true
+        true
+        return true
     }
     /**
      * the setup for a button
@@ -20,8 +20,16 @@ namespace starterkit {
     //% buttonId=starterkittrimpot button="trimpot P0"
     export function trimpot(): void {
         led.plotBarGraph(
-        pins.analogReadPin(AnalogPin.P0),
-        1023
+            pins.analogReadPin(AnalogPin.P0),
+            1023
         )
+    }
+    /**
+     * sets the lights to any color
+     */
+    //% buttonId=starterkitrgb block="set P0 $red | set P1 $green | set P2 $blue"
+    //% red.min=0 red.max=1 green.min=0 green.max=1 blue.min=0 blue.max=1
+    export function rgb(red: number, green: number, blue: number): void {
+        any
     }
 }
